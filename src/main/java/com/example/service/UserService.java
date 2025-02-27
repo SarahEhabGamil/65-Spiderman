0715efbe9d434b4d27de9c4caf521418f407f786
marwantam <marwan.tam18@gmail.com>
2025-02-27 08:56:23 +0200

    user service
    initial user service with all dependency injections

--- src/main/java/com/example/service/UserService.java
+++ src/main/java/com/example/service/UserService.java
@@ -1,4 +1,44 @@
 package com.example.service;
 
+import com.example.model.Order;
+import com.example.model.User;
+import com.example.repository.UserRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+@Service
+@SuppressWarnings("rawtypes")
 public class UserService {
+
+    private UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public User addUser(User user) {
+        return userRepository.addUser(user);
+    }
+    public User getUser(UUID id) {
+        return userRepository.getUserById(id);
+    }
+    public ArrayList<User> getUsers() {
+        return userRepository.getUsers();
+    }
+    public List<Order> getOrdersByUserId(UUID userId) {
+        return userRepository.getOrdersByUserId(userId);
+    }
+    public void addOrderToUser(UUID userId, Order order) {
+        userRepository.addOrderToUser(userId, order);
+    }
+    public void removeOrderFromUser(UUID userId, UUID orderId){
+        userRepository.removeOrderFromUser(userId, orderId);
+    }
+    public void deleteUserById(UUID userId){
+        userRepository.deleteUserById(userId);
+    }
+
 }
